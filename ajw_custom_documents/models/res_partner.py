18ba71e5217389880f1919977d4e647e45ab1c93
Sharan C M <cmsharan.2019@gmail.com>
2026-07-02 17:30:56 +0530

    refactor: use @api.model_create_multi in ajw_custom_documents

--- ajw_custom_documents/models/res_partner.py
+++ ajw_custom_documents/models/res_partner.py
@@ -2,50 +2,63 @@ from odoo import models, fields, api
 
 
 class ResPartner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
     msme_type = fields.Selection(
         selection=[
-            ('micro', 'Micro'),
-            ('small', 'Small'),
-            ('medium', 'Medium'),
-        ], store=True,
-        string='MSME Type',
-        help="Select the type of MSME."
+            ("micro", "Micro"),
+            ("small", "Small"),
+            ("medium", "Medium"),
+        ],
+        string="MSME Type",
+        store=True,
+        help="Select the type of MSME.",
     )
 
-    @api.model
-    def create(self, vals):
-        partner = super(ResPartner, self).create(vals)
+    @api.model_create_multi
+    def create(self, vals_list):
+        partners = super().create(vals_list)
 
-        if 'msme_type' in vals and vals['msme_type']:
-            self._create_document_request(partner)
+        for partner, vals in zip(partners, vals_list):
+            if vals.get("msme_type"):
+                self._create_document_request(partner)
 
-        return partner
+        return partners
 
     def write(self, vals):
-        res = super(ResPartner, self).write(vals)
+        res = super().write(vals)
 
-        if 'msme_type' in vals and vals['msme_type']:
-            for record in self:
-                self._create_document_request(record)
+        if vals.get("msme_type"):
+            for partner in self:
+                self._create_document_request(partner)
 
         return res
 
     def _create_document_request(self, partner):
-        existing_request = self.env['documents.request_wizard'].search([
-            ('partner_id', '=', partner.id),
-        ], limit=1)
+        existing_request = self.env["documents.request_wizard"].search(
+            [("partner_id", "=", partner.id)],
+            limit=1,
+        )
 
-        if not existing_request:
+        if existing_request:
+            return
 
-            default_folder = self.env['documents.document'].search([('name', '=', 'MSME Certificates')], limit=1)
-            if default_folder:
-                request_wizard = self.env['documents.request_wizard'].create({
-                    'name': f"Document Request for {partner.name}",
-                    'requestee_id': self.env.user.partner_id.id,
-                    'partner_id': partner.id,
-                    'folder_id': default_folder.id,
-                    'activity_type_id': self.env.ref('documents.mail_documents_activity_data_md').id,
-                })
-                request_wizard.request_document()
+        default_folder = self.env["documents.document"].search(
+            [("name", "=", "MSME Certificates")],
+            limit=1,
+        )
+
+        if not default_folder:
+            return
+
+        request_wizard = self.env["documents.request_wizard"].create({
+            "name": f"Document Request for {partner.name}",
+            "requestee_id": self.env.user.partner_id.id,
+            "partner_id": partner.id,
+            "folder_id": default_folder.id,
+            "activity_type_id": self.env.ref(
+                "documents.mail_documents_activity_data_md"
+            ).id,
+        })
+
+        request_wizard.request_document()

68242eb6c5075fce7e15631022cc464615b2bda5
Sharan C M <cmsharan.2019@gmail.com>
2026-07-02 17:26:00 +0530

    refactor: use @api.model_create_multi in ajw_stock_orderpoint_autoname

--- ajw_stock_orderpoint_autoname/models/orderpoint.py
+++ ajw_stock_orderpoint_autoname/models/orderpoint.py
@@ -4,16 +4,17 @@ from odoo import models, api
 class StockWarehouseOrderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
-    @api.model
-    def create(self, vals):
-        # Auto-generate name if missing
-        if not vals.get("name"):
-            product = self.env["product.product"].browse(vals.get("product_id"))
-            location = self.env["stock.location"].browse(vals.get("location_id"))
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            # Auto-generate name if missing
+            if not vals.get("name"):
+                product = self.env["product.product"].browse(vals.get("product_id"))
+                location = self.env["stock.location"].browse(vals.get("location_id"))
 
-            if product and location:
-                vals["name"] = f"{product.display_name} - {location.complete_name}"
-            else:
-                vals["name"] = "Reordering Rule"
+                if product and location:
+                    vals["name"] = f"{product.display_name} - {location.complete_name}"
+                else:
+                    vals["name"] = "Reordering Rule"
 
-        return super().create(vals)
+        return super().create(vals_list)
